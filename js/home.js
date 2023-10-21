0b139ee4b75e4e540b2ca405046973d20dfae491
ioniali <83841704+ioniali@users.noreply.github.com>
2023-10-21 15:59:38 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -6,44 +6,68 @@ const searchButton = document.getElementById('search-button');
 
 const summonerArray = [];
 
-function removeItemFromArray(array, item) {
+function removeItemFromArray(array, item){
     const index = array.indexOf(item);
-    if (index !== -1) {
+    if (index !== -1){
         array.splice(index, 1);
     }
 }
 
-function appendSummoner(platformId, summonerName){
-    const summoner = {platformId: platformId, summonerName: summonerName};
+function createGrid(){
     const divItem = document.createElement('div');
     divItem.className = 'grid';
-
-    const labelItem = document.createElement('label');
-    labelItem.textContent = `${platformId} ${summonerName}`;
-    divItem.appendChild(labelItem);
-    
-    const buttomItem = document.createElement('button');
-    buttomItem.textContent = 'Remove';
-    buttomItem.addEventListener('click', () => {
-        summonerList.removeChild(divItem);
-        removeItemFromArray(summonerArray, summoner);
-    });
-    divItem.appendChild(buttomItem);
-
-    summonerArray.push(summoner);
-
-    summonerList.appendChild(divItem);
-    summonerNameInput.value = '';
+    return divItem;
 }
 
-appendButton.addEventListener('click', function() {
+function createLabel(textContent){
+    const labelItem = document.createElement('label');
+    labelItem.textContent = textContent;
+    return labelItem;
+}
+
+function createButton(textContent){
+    const buttonItem = document.createElement('button');
+    buttonItem.textContent = textContent;
+    return buttonItem;
+}
+
+function appendSummoner(platformId, summonerName){
+    const summoner = {platformId: platformId, summonerName: summonerName};
+
+    const gridItem = createGrid();
+    const labelItem = createLabel(`${platformId}\t${summonerName}`);
+    const buttonItem = createButton('Remove');
+
+    buttonItem.addEventListener('click', () => {
+        summonerList.removeChild(gridItem);
+        removeItemFromArray(summonerArray, summoner);
+    });
+
+    gridItem.appendChild(labelItem);
+    gridItem.appendChild(buttonItem);
+    summonerList.appendChild(gridItem);
+    summonerNameInput.value = '';
+
+    summonerArray.push(summoner);
+}
+
+appendButton.addEventListener('click', function(){
     const summonerName = summonerNameInput.value.trim();
     if (summonerName !== ''){
         appendSummoner(regionSelect.value, summonerName);
     }
 });
 
-searchButton.addEventListener('click', function() {
+function getParamsStr(){
+    const params = [];
+    for (const summoner of summonerArray){
+        params.push(`${summoner.platformId}_${summoner.summonerName}`);
+    }
+    return params.join(',');
+}
+
+searchButton.addEventListener('click', function(){
+    const platformId = regionSelect.value;
     const summonerName = summonerNameInput.value.trim();
     if (summonerName === ''){
         if (summonerArray.length > 0){
@@ -51,10 +75,10 @@ searchButton.addEventListener('click', function() {
             for (const summoner of summonerArray){
                 params.push(`${summoner.platformId}_${summoner.summonerName}`);
             }
-            window.location.href  = 'result?summoners=' + params.join(',');
+            window.location.href  = 'result?summoners=' + getParamsStr();
         }
     }
     else {
-        appendSummoner(regionSelect.value, summonerName);
+        appendSummoner(platformId, summonerName);
     }
 });
