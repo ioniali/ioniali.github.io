d763cfaa68fe56bae0305065284c9d3f8d06be75
ioniali <83841704+ioniali@users.noreply.github.com>
2023-11-03 14:17:04 +0000

    a

--- js/result.js
+++ js/result.js
@@ -55,10 +55,10 @@ async function main(){
     for (const summoner of summonerArray){
         if (isPlatformIdValid(summoner.platformId) && isSummonerNameValid(summoner.summonerName)){
             try {
-                const response = await fetch(`https://jarvan.ddns.net/api/data/${summoner.platformId}/${summoner.summonerName}`);
+                const response = await fetch(`https://jarvan.ddns.net/api/player/${summoner.platformId}/${summoner.summonerName}`);
                 const player = await response.json();
                 
-                for (const matchData of [...player.matches['420'], ...player.matches['440']]){
+                for (const matchData of player.score.list){
                     const { championName, individualPosition, score } = matchData;
                     if (!championArray.includes(championName)){
                         for (const position of positionArray){
